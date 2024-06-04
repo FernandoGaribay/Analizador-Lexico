fd340964ad98bac9b21677daa71305ac79b8f90e
JosephQuintero <josephcarrasco15082@gmail.com>
2024-06-04 11:27:16 -0600

    + Creacion de Automata NumeroDecimal

--- src/automatas/NumeroDecimal.java
+++ src/automatas/NumeroDecimal.java
@@ -1,10 +1,9 @@
-
 package automatas;
 
 import java.util.HashMap;
 
 public class NumeroDecimal {
-    
+
     private static final HashMap<String, HashMap<Character, String>> transiciones;
 
     private static final String q0 = "q0";
@@ -13,36 +12,61 @@ public class NumeroDecimal {
     private static final String q3 = "q3";
     private static final String q4 = "q4";
     private static final String q5 = "q5";
-    
+
     static {
         transiciones = new HashMap<>();
 
         HashMap<Character, String> q0Transitions = new HashMap<>();
         q0Transitions.put('1', q2);
         q0Transitions.put('-', q1);
+        q0Transitions.put('.', q5);
         transiciones.put(q0, q0Transitions);
 
         HashMap<Character, String> q1Transitions = new HashMap<>();
-        q1Transitions.put('+', q2);
+        q1Transitions.put('1', q2);
+        q1Transitions.put('-', q5);
+        q1Transitions.put('.', q5);
         transiciones.put(q1, q1Transitions);
 
         //Estado de aceptacion q2
         HashMap<Character, String> q2Transitions = new HashMap<>();
-        q2Transitions.put('+', q3);
+        q2Transitions.put('1', q2);
+        q2Transitions.put('-', q5);
+        q2Transitions.put('.', q3);
         transiciones.put(q2, q2Transitions);
 
-        //Estado basura q3
         HashMap<Character, String> q3Transitions = new HashMap<>();
-        q3Transitions.put('+', q3);
+        q3Transitions.put('1', q4);
+        q3Transitions.put('-', q5);
+        q3Transitions.put('.', q5);
         transiciones.put(q3, q3Transitions);
+
+        //Estado de aceptacion q4
+        HashMap<Character, String> q4Transitions = new HashMap<>();
+        q4Transitions.put('1', q4);
+        q4Transitions.put('-', q5);
+        q4Transitions.put('.', q5);
+        transiciones.put(q4, q4Transitions);
+
+        //Estado basura q5
+        HashMap<Character, String> q5Transitions = new HashMap<>();
+        q5Transitions.put('1', q5);
+        q5Transitions.put('-', q5);
+        q5Transitions.put('.', q5);
+        transiciones.put(q5, q5Transitions);
     }
 
-    public static boolean validarIncremento(String input) {
+    public static boolean validarNumeroDecimal(String input) {
         String currentState = q0;
         System.out.println("λ -> " + currentState);
+        boolean estadosAceptacion = false;
+
+        //Funcion for:
         
-        //Funcion for
-        
-        return currentState.equals(q2);
+        if (currentState.equals(q2) || currentState.equals(q4)) {
+            estadosAceptacion = true;
+        }
+
+        return estadosAceptacion;
     }
 }
